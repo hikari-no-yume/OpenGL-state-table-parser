06f122244bf75b178eff21014395c710abb68204
hikari_no_yume <hikari@noyu.me>
2023-04-29 18:05:02 +0200

    Move cell matching into its own function

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,45 @@
 #![allow(non_snake_case)] // let me capitalize the crate name, Rust!
 
+/// Match a set of curly braces potentially containing nested curly braces.
+/// Returns the content of the outermost set of braces, and the remaining text.
+fn read_cell(text: &str) -> (&str, &str) {
+    let mut offset = 0;
+    let mut depth: u32 = 0;
+    loop {
+        offset += text[offset..].find(['{', '}']).unwrap();
+        if text[offset..].starts_with('{') {
+            offset += 1;
+            depth += 1;
+        } else {
+            assert!(text[offset..].starts_with('}'));
+            offset += 1;
+            depth -= 1;
+            if depth == 0 {
+                break;
+            }
+        }
+    }
+    let (cell, remainder) = text.split_at(offset);
+    let cell = cell
+        .trim()
+        .strip_prefix('{')
+        .unwrap()
+        .strip_suffix('}')
+        .unwrap();
+    (cell, remainder)
+}
+
 fn main() {
     let mut args = std::env::args();
     // Skip argv[0] (binary name)
     args.next();
 
-    let filename = args.next().expect("Please specify a filename and number of columns");
-    let column_count = args.next().expect("Please specify a filename and number of columns");
+    let filename = args
+        .next()
+        .expect("Please specify a filename and number of columns");
+    let column_count = args
+        .next()
+        .expect("Please specify a filename and number of columns");
     let column_count: u32 = column_count.parse().expect("Invalid column count");
     if args.next().is_some() {
         panic!("Too many arguments");
@@ -38,32 +71,13 @@ fn main() {
         text = &text[entry_offset..];
         text = &text[text.find('{').unwrap()..];
 
-        // Match cells with nested parentheses
         let mut cells = Vec::new();
-        let mut offset = 0;
         for _ in 0..column_count {
-            let start_offset = offset;
-            let mut depth: u32 = 0;
-            loop {
-                offset += text[offset..].find(['{', '}']).unwrap();
-                if text[offset..].starts_with('{') {
-                    offset += 1;
-                    depth += 1;
-                } else {
-                    assert!(text[offset..].starts_with('}'));
-                    offset += 1;
-                    depth -= 1;
-                    if depth == 0 {
-                        break;
-                    }
-                }
-            }
-            let cell = text[start_offset..offset].trim();
-            let cell = cell.strip_prefix('{').unwrap().strip_suffix('}').unwrap();
+            let (cell, new_text) = read_cell(text);
             cells.push(cell.to_string());
+            text = new_text;
         }
         entries.push(cells);
-        text = &text[offset..];
     }
 
     println!("<table>");
